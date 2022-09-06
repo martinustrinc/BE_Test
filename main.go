bd07c004778bd8eedbe9883b8bf38ea7cc7af9d0
Martinus Tri Nur Cahyono <martinustrinc@gmail.com>
2022-09-06 21:47:42 +0700

    upload code

---
+++ main.go
@@ -0,0 +1,16 @@
+package BE_Test
+
+import (
+	"github.com/martinustrinc/BE_Test"
+	//"github.com/gin-gonic/gin"
+	//"os"
+)
+
+func main() {
+	//port := os.Getenv("PORT")
+	//if port == ""{
+	//	port = "9000"
+	//}
+
+	Shopping.Run()
+}
